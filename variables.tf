ecc62b780a39c6b28850fcf7003e82b206eb2426
Annie Hawes <annie.hawes@nationalarchives.gov.uk>
2023-10-10 10:38:52 +0100

    Remove obsolete variable

--- variables.tf
+++ variables.tf
@@ -12,8 +12,3 @@ variable "tre_permission_boundary_arn" {
   description = "ARN of the TRE permission boundary policy"
   type        = string
 }
-
-variable "eventbus_topic_arn" {
-  description = "ARN of the event bus topic to subscribe to"
-  type        = string
-}
